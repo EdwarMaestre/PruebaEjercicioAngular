79a18af3e0a5d05e63b4d0033265c0c861fc7e4d
Edwar <edwar2904@gmail.com>
2019-07-07 14:47:36 -0500

    PruebaRealizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopaginaencontradaComponent } from './shared/nopaginaencontrada/nopaginaencontrada.component';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { ProgressComponent } from './paginas/progress/progress.component';
+import { Graficas1Component } from './paginas/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopaginaencontradaComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
